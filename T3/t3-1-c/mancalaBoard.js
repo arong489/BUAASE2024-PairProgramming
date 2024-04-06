1c49dc81d3785052656f94b554b7f1bec84a7250
arong489 <112385316+arong489@users.noreply.github.com>
2024-04-06 23:13:06 +0800

    [T3.2]:version1.1 glue code fix

--- T3/t3-1-c/mancalaBoard.js
+++ T3/t3-1-c/mancalaBoard.js
@@ -2,38 +2,38 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const fs = require('fs');
-const wasmSource = new Uint8Array(fs.readFileSync("./t3-2-c/mancalaOperator.wasm"));
+const wasmSource = new Uint8Array(fs.readFileSync("./t3-1-c/mancalaBoard.wasm"));
 const wasmModule = new WebAssembly.Module(wasmSource);
 const wasmInstance = new WebAssembly.Instance(wasmModule, {
   env: {
-    // emscripten_resize_heap: function (requestedSize) {
-    //   let oldSize = HEAPU8.length;
-    //   requestedSize = requestedSize >>> 0;
-    //   let maxHeapSize = 2147483648;
-    //   if (requestedSize > maxHeapSize) {
-    //     return false;
-    //   }
-    //   for (let cutDown = 1; cutDown <= 4; cutDown *= 2) {
-    //     let overGrownHeapSize = oldSize * (1 + 0.2 / cutDown);
-    //     overGrownHeapSize = Math.min(
-    //       overGrownHeapSize,
-    //       requestedSize + 100663296
-    //     );
-    //     let newSize = Math.min(
-    //       maxHeapSize,
-    //       alignUp(Math.max(requestedSize, overGrownHeapSize), 65536)
-    //     );
-    //     let replacement = emscripten_realloc_buffer(newSize);
-    //     if (replacement) {
-    //       return true;
-    //     }
-    //   }
-    //   return false;
-    // }
+    emscripten_resize_heap: function (requestedSize) {
+      let oldSize = HEAPU8.length;
+      requestedSize = requestedSize >>> 0;
+      let maxHeapSize = 2147483648;
+      if (requestedSize > maxHeapSize) {
+        return false;
+      }
+      for (let cutDown = 1; cutDown <= 4; cutDown *= 2) {
+        let overGrownHeapSize = oldSize * (1 + 0.2 / cutDown);
+        overGrownHeapSize = Math.min(
+          overGrownHeapSize,
+          requestedSize + 100663296
+        );
+        let newSize = Math.min(
+          maxHeapSize,
+          alignUp(Math.max(requestedSize, overGrownHeapSize), 65536)
+        );
+        let replacement = emscripten_realloc_buffer(newSize);
+        if (replacement) {
+          return true;
+        }
+      }
+      return false;
+    }
   }
 });
 
-export function mancalaOperator(flag, seq) {
+export function mancalaBoard(flag, seq, size) {
   // transform
   const savedStack = wasmInstance.exports.stackSave();
   const ptr = wasmInstance.exports.stackAlloc(seq.length * 4);
@@ -44,7 +44,15 @@ export function mancalaOperator(flag, seq) {
     heap[start + i] = seq[i];
   }
 
-  const result = wasmInstance.exports.mancalaOperator(flag, ptr);
+  const result = wasmInstance.exports.mancalaBoard(flag, ptr, size);
+  const data = new Uint32Array(wasmInstance.exports.memory.buffer, result, 15);
+  const ret = Array.from(data);
 
-  return result;
+  wasmInstance.exports.stackRestore(savedStack);
+  
+  console.log(ret.slice(7,13).reverse())
+  console.log(ret.slice(0,6))
+  console.log("Player1: %d    Player2: %d    Next: %d", ret[6], ret[13], ret[14])
+
+  return ret;
 }
